614a6ff6a4932a85618a6c50a201836570c9216b
MrWaknis <mihirwaknis282@gmail.com>
2015-10-24 15:55:48 -0700

    Added basic Xcode Project

---
+++ Ludus/Ludus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ludus
+//
+//  Created by Mihir Waknis on 10/24/15.
+//  Copyright © 2015 Ludus Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
